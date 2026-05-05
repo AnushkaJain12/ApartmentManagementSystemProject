0ba0083c1d9c2163fbdb9a023fa22d6528f717f8
wadhwaaanjali <wadhwaanjali640@gmail.com>
2026-05-05 21:01:19 +0530

    minor fixes

--- public/js/main.js
+++ public/js/main.js
@@ -826,6 +826,9 @@ document.getElementById('allot-form').addEventListener('submit', async (e) => {
             hideModal('allotModal');
             fetchApartments();
             fetchActivities(); // Refresh activities
+        } else {
+            const errData = await res.json();
+            alert('Failed to update allotment: ' + (errData.message || 'Unknown error'));
         }
     } catch (err) {
         alert('Error updating allotment');
